046d1cc505b746d6b9075f6128fa2c9a8cae710d
Paul Dunaway <pauldunaway@yahoo.com>
2020-11-09 00:13:50 -0500

    react_week_5

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,28 @@
-import { createStore } from 'redux';
-import { Reducer, initialState } from './reducer';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import { createForms } from 'react-redux-form';
+import { Campsites } from './campsites';
+import { Comments } from './comments';
+import { Partners } from './partners';
+import { Promotions } from './promotions';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { InitialFeedback } from './forms';
+import { composeWithDevTools } from 'redux-devtools-extension'
+
 
 export const ConfigureStore = () => {
     const store = createStore(
-        Reducer,
-        initialState
+        combineReducers({
+            campsites: Campsites,
+            comments: Comments,
+            partners: Partners,
+            promotions: Promotions,
+            ...createForms({
+                feedbackForm: InitialFeedback
+            })
+        }),
+        composeWithDevTools(applyMiddleware(thunk, logger))
     );
 
     return store;
-}
+};
